85574a5a09771020af1adc358c21aee072619102
Jeff Paletta <jeffpaletta@gmail.com>
2016-11-02 01:12:21 -0400

    experience almost all working except workshop when all opened

--- js/script.js
+++ js/script.js
@@ -413,24 +413,66 @@ $(document).ready(function() {
 	$('body').on('click', '.read-more-toggle-shinola', function(e) {
 		$('.read-more-content-shinola').toggleClass('hide');
 		$('.read-more-toggle-shinola').toggleClass('hide');
-// 		$('#about-caption-deprogram-2').addClass('move-down');
+		$('#about-caption-practicum').addClass('shinola-4practicum-adjust');
+		$('#about-caption-workshop').removeClass('workshop-adjust');
+		$('#about-caption-workshop').addClass('shinola-4workshop-adjust');
 		e.preventDefault();
 	});
+	
 	$('body').on('click', '.read-more-toggle-deprogram', function(e) {
 		$('.read-more-content-deprogram').toggleClass('hide');
 		$('.read-more-toggle-deprogram').toggleClass('hide');
+		$('#about-caption-workshop').addClass('deprogram-4workshop-adjust');
+		
+		if($('#about-caption-workshop').hasClass('practicum-4workshop-adjust') && ('#about-caption-workshop').hasClass('shinola-4workshop-adjust')) {
+			$('#about-caption-workshop').removeClass('shinola-4workshop-adjust');
+			$('#about-caption-workshop').removeClass('practicum-4workshop-adjust');
+			$('#about-caption-workshop').addClass('deprogram-shinola-practicum-4workshop-adjust');
+		}
+		else {
+			if( $('#about-caption-practicum').hasClass('shinola-4practicum-adjust')) {
+				$('#about-caption-practicum').removeClass('shinola-4practicum-adjust');
+				$('#about-caption-practicum').addClass('deprogram-shinola-4practicum-adjust');
+			}
+			else {
+				$('#about-caption-practicum').addClass('deprogram-4practicum-adjust');
+			}
+			
+			
+			if( $('#about-caption-workshop').hasClass('practicum-4workshop-adjust')) {
+				$('#about-caption-workshop').removeClass('practicum-4workshop-adjust');
+				$('#about-caption-workshop').addClass('deprogram-practicum-4workshop-adjust');
+			}
+			else {
+				$('#about-caption-workshop').addClass('deprogram-4workshop-adjust');
+			}
+			
+			if( $('#about-caption-workshop').hasClass('shinola-4workshop-adjust')) {
+				$('#about-caption-workshop').removeClass('shinola-4workshop-adjust');
+				$('#about-caption-workshop').addClass('deprogram-shinola-4workshop-adjust');
+			}
+			else {
+				$('#about-caption-workshop').addClass('deprogram-4workshop-adjust');
+			}
+			
+		}
 		e.preventDefault();
 	});
+	
 	$('body').on('click', '.read-more-toggle-practicum', function(e) {
 		$('.read-more-content-practicum').toggleClass('hide');
 		$('.read-more-toggle-practicum').toggleClass('hide');
+		$('#about-caption-workshop').addClass('practicum-4workshop-adjust');
 		e.preventDefault();
 	});
+	
 	$('body').on('click', '.read-more-toggle-workshop', function(e) {
 		$('.read-more-content-workshop').toggleClass('hide');
 		$('.read-more-toggle-workshop').toggleClass('hide');
 		e.preventDefault();
 	});
+	
+	
 	
 	// Freelance 
 	$('body').on('click', '.read-more-toggle-freelance', function(e) {
@@ -438,16 +480,19 @@ $(document).ready(function() {
 		$('.read-more-toggle-freelance').addClass('hide');
 		e.preventDefault();
 	});
+	
 	$('body').on('click', '.read-more-toggle-heavenly', function(e) {
 		$('.read-more-content-heavenly').toggleClass('hide');
 		$('.read-more-toggle-heavenly').addClass('hide');
 		e.preventDefault();
 	});
+	
 	$('body').on('click', '.read-more-toggle-faceplay', function(e) {
 		$('.read-more-content-faceplay').toggleClass('hide');
 		$('.read-more-toggle-faceplay').addClass('hide');
 		e.preventDefault();
 	});
+	
 	$('body').on('click', '.read-more-toggle-sign', function(e) {
 		$('.read-more-content-sign').toggleClass('hide');
 		$('.read-more-toggle-sign').addClass('hide');
