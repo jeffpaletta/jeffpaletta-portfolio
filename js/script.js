52e0db4e7077e1aa0fe155713b28f6935e33c4e7
Jeff Paletta <jeffpaletta@gmail.com>
2016-11-02 12:37:14 -0400

    Revert "made significant progress"
    This reverts commit 9f44b2d3577c4e6d8bf346a0a432a911c1e8344a.

--- js/script.js
+++ js/script.js
@@ -400,6 +400,7 @@ $(document).ready(function() {
 		$('.gallery').removeClass('visible');
 		clicked = false;
 	});
+	
 	// Resume Info Togglers
 	$('body').on('click', '.show-more-toggle', function(e) {
 		$('.show-more-content').removeClass('hide');
@@ -408,33 +409,78 @@ $(document).ready(function() {
 		$('.show-more-freelance').addClass('hide');
 		e.preventDefault();
 	});
-	//Experience
-	// Shinola Practicum Show More Toggler
+	
+	// Experience
 	$('body').on('click', '.read-more-toggle-shinola', function(e) {
 		$('.read-more-content-shinola').toggleClass('hide');
 		$('.read-more-toggle-shinola').toggleClass('hide');
+		$('#about-caption-practicum').addClass('shinola-4practicum-adjust');
+		$('#about-caption-workshop').removeClass('workshop-adjust');
+		$('#about-caption-workshop').addClass('shinola-4workshop-adjust');
 		e.preventDefault();
 	});
-	// De Program Show More Toggler
+	
 	$('body').on('click', '.read-more-toggle-deprogram', function(e) {
 		$('.read-more-content-deprogram').toggleClass('hide');
 		$('.read-more-toggle-deprogram').toggleClass('hide');
+		$('#about-caption-workshop').addClass('deprogram-4workshop-adjust');
+		
+		if( $('#about-caption-practicum').hasClass('shinola-4practicum-adjust')) {
+			$('#about-caption-practicum').removeClass('shinola-4practicum-adjust');
+			$('#about-caption-practicum').addClass('deprogram-shinola-4practicum-adjust');
+		}
+		else {
+			$('#about-caption-practicum').addClass('deprogram-4practicum-adjust');
+		}
+		
+		
+		if( $('#about-caption-workshop').hasClass('practicum-4workshop-adjust')) {
+			$('#about-caption-workshop').removeClass('practicum-4workshop-adjust');
+			$('#about-caption-workshop').addClass('deprogram-practicum-4workshop-adjust');
+		}
+		else {
+			$('#about-caption-workshop').addClass('deprogram-4workshop-adjust');
+		}
+		
+		if( $('#about-caption-workshop').hasClass('shinola-4workshop-adjust')) {
+			$('#about-caption-workshop').removeClass('shinola-4workshop-adjust');
+			$('#about-caption-workshop').addClass('deprogram-shinola-4workshop-adjust');
+		}
+		else {
+			$('#about-caption-workshop').addClass('deprogram-4workshop-adjust');
+		}
+		
+/*
+		if($('#about-caption-workshop').hasClass('practicum-4workshop-adjust') && ('#about-caption-workshop').hasClass('shinola-4workshop-adjust')) {
+			$('#about-caption-workshop').removeClass('shinola-4workshop-adjust');
+			$('#about-caption-workshop').removeClass('practicum-4workshop-adjust');
+			$('#about-caption-workshop').addClass('deprogram-shinola-practicum-4workshop-adjust');
+		}
+		else {
+			
+		}
+*/
 		e.preventDefault();
 	});
-	// Department Practicum Show More Toggler
+	
 	$('body').on('click', '.read-more-toggle-practicum', function(e) {
 		$('.read-more-content-practicum').toggleClass('hide');
 		$('.read-more-toggle-practicum').toggleClass('hide');
+		$('#about-caption-workshop').addClass('practicum-4workshop-adjust');
 		e.preventDefault();
 	});
-	// Prototyping Workshop Show More Toggler
+	
 	$('body').on('click', '.read-more-toggle-workshop', function(e) {
-		$('.read-more-content-workshop').toggleClass('hide');
+		$('.show-more-content-workshop').toggleClass('hide');
 		$('.read-more-toggle-workshop').toggleClass('hide');
 		e.preventDefault();
 	});
 	
-
+	
+	
+	
+	
+	
 	
 	// Freelance 
 	
